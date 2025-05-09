6a0dec76f07cff7dc25e99c365c7b374ea7b1dfd
Sarayut Senarsa <119925244+l3lack30@users.noreply.github.com>
2025-05-09 13:26:00 +0700

    Refactor error responses in authorization middleware to include 'data: null' for consistency

--- middleware/token.authorizeRoles.js
+++ middleware/token.authorizeRoles.js
@@ -2,7 +2,7 @@ const authorizeRoles = (...allowedRoles) => (req, res, next) => {
     const user = req.user; 
 
     if (!user || !allowedRoles.includes(user.role)) {
-        return res.status(403).json({ status: 403, message: 'Forbidden: Admin access only.' });
+        return res.status(403).json({ status: 403, message: 'Forbidden: Admin access only.', data: null });
     }
 
     next();
--- middleware/token.middleware.js
+++ middleware/token.middleware.js
@@ -4,10 +4,10 @@ const authenticateToken = (req, res, next) => {
     const authHeader = req.headers['authorization']; // 'Bearer <token>'
     const token = authHeader && authHeader.split(' ')[1];
 
-    if (!token) return res.status(401).json({ status: 401, message: 'Access token required' });
+    if (!token) return res.status(401).json({ status: 401, message: 'Access token required', data: null });
 
     jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-        if (err) return res.status(403).json({ status: 403, message: 'Invalid token' });
+        if (err) return res.status(403).json({ status: 403, message: 'Invalid token', data: null });
         req.user = user; // ถ้า token ถูกต้อง จะมีข้อมูล user ที่ถูกเข้ารหัสอยู่ใน token
         next();
     });
